b011f9c5ee9254bd683c826e36f9e11433c8d850
Vlada <vladapopov02@gmail.com>
2023-01-08 16:28:06 +0100

    added crossover and mutation

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ def init(n, population_n):
 
     for i in range(population_n):
         for j in range(n):
-            population[i][j]["x"] = np.random.randint(0, n - 1)
-            population[i][j]["y"] = np.random.randint(0, n - 1)
+            population[i][j]["x"] = np.random.randint(0, n)
+            population[i][j]["y"] = np.random.randint(0, n)
 
     return population
 
@@ -34,28 +34,80 @@ def fitness_score(individual):
             elif individual[i]["y"] == individual[j]["y"]:
                 res += 1
 
-            elif np.abs(individual[i]["x"] - individual[j]["x"]) == np.abs(individual[i]["y"] - individual[j]["y"]):
+            elif np.abs(int(individual[i]["x"]) - int(individual[j]["x"])) == np.abs(
+                int(individual[i]["y"]) - int(individual[j]["y"])
+            ):
                 res += 1
 
     # na ovaj nacin brojimo svako napadanje dvaput pa delimo konacnan rezultat sa 2
     return res / 2
 
 
+def crossover(n, parent1, parent2):
+    # nasumicni pivoting point
+    crossover_point = np.random.randint(0, n)
+
+    child1 = np.concatenate((parent1[:crossover_point], parent2[crossover_point:]))
+    child2 = np.concatenate((parent2[:crossover_point], parent1[crossover_point:]))
+
+    # mutacija - 70% sanse za random promenu u detetu
+    if np.random.random() > 0.3:
+        mutation_point = np.random.randint(0, n)
+        child1[mutation_point]["x"] = np.random.randint(0, n)
+        child1[mutation_point]["y"] = np.random.randint(0, n)
+
+    if np.random.random() > 0.3:
+        mutation_point = np.random.randint(0, n)
+        child2[mutation_point]["x"] = np.random.randint(0, n)
+        child2[mutation_point]["y"] = np.random.randint(0, n)
+
+    return child1, child2
+
+
 def genetic(n, population_n=100):
+    generation = 0
+    dtype = np.dtype([("x", np.uint8), ("y", np.uint8)])
+
     # inicijalizacija - kodiranje jedinki
     population = init(n, population_n)
 
-    # sortiranje po prilagodjenosti
-    population = np.array(sorted(population, key=lambda ind: fitness_score(ind) * np.random.random()))
+    while True:
+        # pravljenje dece - ukrstanje
+        children = np.zeros((population_n, n), dtype)
+        for i in range(int(population_n / 2)):
+            # sortiranje po prilagodjenosti - selekcija
+            population = np.array(sorted(population, key=lambda ind: fitness_score(ind) * np.random.random()))
 
-    print(population)
+            child1, child2 = crossover(n, population[0], population[1])
+            children[i * 2] = child1
+            children[i * 2 + 1] = child2
+
+        generation += 1
+
+        # pustamo 5% najboljih roditelja da prezive - elitizam
+        elitism_deg = int(population_n * 0.05)
+        children = np.array(sorted(children, key=lambda ind: fitness_score(ind)))
+        population = np.array(sorted(population, key=lambda ind: fitness_score(ind)))
+
+        for i in range(elitism_deg):
+            if fitness_score(children[len(children) - i - 1]) > fitness_score(population[i]):
+                children[len(children) - i - 1], population[i] = population[i], children[len(children) - i - 1]
+
+        print(fitness_score(children[0]))
+        if fitness_score(children[0]) == 0:
+            return children[0], generation
+
+        population = children
 
 
 def main():
     print("Unesite n")
     n = int(input(">> "))
 
-    queens_array = genetic(n)
+    optimal_solution, generations = genetic(n, 50)
+
+    print("Broj generacija", generations)
+    print("Resenje", optimal_solution)
 
 
 if __name__ == "__main__":
